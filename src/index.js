fd408841b9c31524f4286aea4140ed5debf0d81f
Max Aubain <max.aubain@gmail.com>
2019-06-24 15:16:36 +0200

    Added Header and Footer .jsx files and to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import Hello from "./Hello"
+import Header from "./Header"
+import Footer from "./Footer"
 
-const HelloWorld = () => {
-    return <Hello />
+const App = () => {
+    return (
+    <div>
+        <Header />
+        <Hello />
+        <Footer />
+    </div>
+    )
 };
 
-ReactDOM.render(<HelloWorld />, document.getElementById("app"))
+ReactDOM.render(<App/>, document.getElementById("app"))
