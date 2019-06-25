020953507557869882cef0adccf00909f4d34e6c
Max Aubain <max.aubain@gmail.com>
2019-06-25 10:05:35 +0200

    Installed React Router and added About and Projects pages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom"
 import Hello from "./Hello"
 import Header from "./Header"
 import Footer from "./Footer"
-import './css/tailwind.css';
+import Projects from "./Projects"
+import About from "./About"
+
+import { BrowserRouter } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
+import './css/tailwind.css'
 
 const App = () => {
     return (
@@ -11,12 +16,20 @@ const App = () => {
             <div className="page-content">
                 <Header />
                 <div className="content-wrapper">
-                    <Hello />
+                    <Switch>
+                        <Route exact path='/' component={Hello}></Route>
+                        <Route exact path='/about' component={About}></Route>
+                        <Route exact path='/projects' component={Projects}></Route>
+                    </Switch>
                 </div>
                 <Footer />
             </div >
         </div >
-)
+    )
 };
 
-ReactDOM.render(<App/>, document.getElementById("app"))
+ReactDOM.render((
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+), document.getElementById("app"))
