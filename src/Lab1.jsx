fa60d7ed81dad2885e6a7f1ebf09d735b4c37a0e
Max Aubain <max.aubain@gmail.com>
2019-06-28 17:38:12 +0200

    Reworking Lab1, has working flashing arrow, text and button, button is nonfunctional

--- src/Lab1.jsx
+++ src/Lab1.jsx
@@ -7,18 +7,37 @@ const Box = posed.div({
 })
 
 class Lab1 extends Component {
-    state = { isVisible: true };
-    
+    constructor(){
+        super()
+        this.state = { isVisible: null };
+    }
+
     componentDidMount() {
         setInterval(() => {
           this.setState({ isVisible: !this.state.isVisible });
         }, 1000);
-      }
-    
-      render() {
-        const { isVisible } = this.state;
-        return <Box className="box" pose={isVisible ? 'visible' : 'hidden'} />;
-      }
+    }
+
+    changeState() {
+        x += 1
+    }
+
+    render() {
+        
+        let { isVisible } = this.state;
+
+        return(
+            <div>
+                <div>
+                    <Box className="up-triangle" pose={isVisible ? 'visible' : 'hidden'} />
+                </div>
+                <div className="text-2xl">
+                    <p style={{maxWidth: '40vw', verticalAlign: 'center' }}>Find your way back to Home without using the browser navigation buttons.</p> 
+                    <button onClick={this.changeState} className="text-white bg-black rounded">Hint?</button>
+                </div>
+            </div>
+        )
+    }
 };
 
 export default Lab1
