480932ddb503d72419de98ea953d06baabbc86b8
Max Aubain <max.aubain@gmail.com>
2019-07-31 18:50:42 +0200

    Adds About information and styling to 'Home'.

--- src/Home.js
+++ src/Home.js
@@ -4,18 +4,23 @@ const Home = () => {
   return (
     <div class='homepage'>
       <div class='section header'>
-        <p>Headshot</p>
+        <img class='headshot' src='./src/img/headShot.png' />
       </div>
       <div class='section content'>
-        <p>about me</p>
-      </div>
+        <p>
+          I am a dedicated engineer with over 10 years of experience, passionate about solving technical
+          and business problems.  To that end, I enjoy leading teams to success and continually
+          improving my own skills.  My career goals are currently focused on web-based application development
+          and managing projects with a team of engineers.
+        </p>
+      </div >
       <div class='section feature-1'>
         <p>Section Feature 1</p>
       </div>
       <div class='section feature-2'>
         <p>Section Feature 2</p>
       </div>
-    </div>
+    </div >
   )
 }
 
