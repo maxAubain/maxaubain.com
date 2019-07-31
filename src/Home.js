a5dd1222bb8ad5d35516a8ccff6cb133def695c4
Max Aubain <max.aubain@gmail.com>
2019-07-31 17:57:23 +0200

    Refactor 'Home' and 'tailwind.src.css' to only include two features, About, and Headshot.

--- src/Home.js
+++ src/Home.js
@@ -2,25 +2,18 @@ import React from "react"
 
 const Home = () => {
   return (
-    <div class='page'>
-      <div class='section menu'></div>
+    <div class='homepage'>
       <div class='section header'>
-        <p>Section Header</p>
+        <p>Headshot</p>
       </div>
       <div class='section content'>
-        <p>Section Content</p>
-      </div>
-      <div class='section sign-up'>
-        <p>Section Sign Up</p>
+        <p>about me</p>
       </div>
       <div class='section feature-1'>
         <p>Section Feature 1</p>
       </div>
       <div class='section feature-2'>
         <p>Section Feature 2</p>
-      </div>
-      <div class='section feature-3'>
-        <p>Section Feature 3</p>
       </div>
     </div>
   )
