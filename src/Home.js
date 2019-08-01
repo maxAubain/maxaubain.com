cc35be7b9080125b001b9f650c63ff99904cecb2
Max Aubain <max.aubain@gmail.com>
2019-08-01 17:21:30 +0200

    Adds headline with profession discriptions and sets headshot and headline side by side.

--- src/Home.js
+++ src/Home.js
@@ -6,12 +6,20 @@ const Home = () => {
       <div class='section header'>
         <img class='headshot' src='./src/img/headShot.png' />
       </div>
+      <div class='section headline'>
+        <h1>Max Aubain</h1>
+        <h2><ul>
+          <li>Full stack developer</li>
+          <li>Project manager</li>
+          <li>Data analyst</li>
+        </ul></h2>
+      </div>
       <div class='section content'>
         <p>
           I am a dedicated engineer with over 10 years of experience, passionate about solving technical
           and business problems.  I enjoy leading teams to success and continually
-          improving my own skills.  My career goals are currently focused on web-based application development
-          and project management.
+          improving my own skills.  My career goals are currently focused on web-based application development, 
+          project management, and data processing.
         </p>
       </div >
       <div class='section feature-1'>
