14a4cbd0295459148f42a0fa97470a122319155b
Max Aubain <max.aubain@gmail.com>
2019-08-01 22:19:07 +0200

    Adds deployment link and readme link to 'Tile.jsx' and 'projects.json', removes text decoration from a href links.

--- src/Tile.jsx
+++ src/Tile.jsx
@@ -5,22 +5,16 @@ import './css/tailwind.src.css';
 const Tile = (props) => {
   let project = props.project
   return (
-
     <div key={project.id} style={{ transition: 'all 0.1s linear' }} className="rounded-lg border border-grey hover:shadow-2xl hover:bg-grey-lightest">
-      <NavLink className="no-underline" to={project.navlink}>
-
-        <img src={project.image} className="w-60 rounded-lg m-2" style={{ height: '150px', objectFit: 'cover' }} />
-
-        <div className="font-bold text-xl text-black px-2">{project.name}</div>
-
-        <div className="px-2 py-4" style={{ minHeight: '100px' }} >
-
-          <p className="text-grey-darker">
-            {project.description}
-          </p>
-        </div>
-
-      </NavLink>
+      <img src={project.image} className="w-60 rounded-lg m-2" style={{ height: '150px', objectFit: 'cover' }} />
+      <div className="font-bold text-xl text-black px-2">{project.name}</div>
+      <div className="px-2 py-4" style={{ minHeight: '100px' }} >
+        <p className="text-grey-darker">
+          {project.description}
+        </p>
+      </div>
+      <div><a href={project.deploy_link}>Deployed Site</a></div>
+      <div><a href={project.readme_link}>Project Documentation</a></div>
     </div>
   )
 }
