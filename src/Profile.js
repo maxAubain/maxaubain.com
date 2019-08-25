6e82fd67b2921ba949d302fca43b677be2049556
Max Aubain <max.aubain@gmail.com>
2019-08-25 10:45:05 +0200

    Adds content to Profile page

--- src/Profile.js
+++ src/Profile.js
@@ -6,23 +6,44 @@ const Profile = () => {
       <div class="section header">
         <div>
           <img class="headshot" src="./src/img/headShot.png" />
-        </div><br></br>
+        </div>
+        <br></br>
         <div>Max Aubain, Ph.D.</div>
       </div>
       <div class="section headline">
-        <a href="./#project-manager" class="feature-ref-1">Project Manager</a>
-        <a href="./#full-stack-developer" class="feature-ref-2">Full Stack Developer</a>
-        <a href="./#data-analyst" class="feature-ref-3">Data Analyst</a>
+        <a href="./#project-manager" class="feature-ref-1">
+          Project Manager
+        </a>
+        <a href="./#full-stack-developer" class="feature-ref-2">
+          Full Stack Developer
+        </a>
+        <a href="./#data-analyst" class="feature-ref-3">
+          Data Analyst
+        </a>
       </div>
       <div class="section content">
-        <p>I am a motivated team player and an advocate for getting the job done right; I have a proven
-        record of generating group cohesion and business value in interdisciplinary, technical projects; 
-        and I enjoy learning and applying emerging technologies.
-        </p>
+        <ul>
+          <li>
+            I am a motivated team player and an advocate for getting the job
+            done right.
+          </li>
+          <br></br>
+          <li>
+            I have a proven record of generating group cohesion and business
+            value in interdisciplinary, technical projects.
+          </li>
+          <br></br>
+          <li>
+            I have over 10 years of experience researching, developing, and
+            applying emerging technologies.
+          </li>
+        </ul>
       </div>
       <div class="content feature" id="project-manager">
         <div class="category">Project Manager</div>
-        <div class="description">managing a large number of things.  a great many things!</div>
+        <div class="description">
+          managing a large number of things. a great many things!
+        </div>
       </div>
       <div class="content feature" id="full-stack-developer">
         hello world
--- src/features/layout.md
+++ src/features/layout.md
@@ -1,12 +1,13 @@
 Action items
-* Add navbar with hover over effect and indicator of current page.
-* Have to-section buttons do a slow move effect.
 * Have section content be mobile friendly.
 * Have all sections adopt the same center style as the profile page.
+Later
+* Add navbar with hover over effect and indicator of current page.
 Done
 * Refactor "Lab2" component name, "Home" to "Profile".
 * Add pictures for PM, Developer, Data Analyst to reflect content.
 * Remove color change with css media queries.
-* Keep navbar on top at all times, contain to same width as body content.
+* Have navbar contain to same width as body content.
 * Refactor components into a folder.
 * Have Travel have a "coming soon" message.
+* Have to-section buttons do a slow move effect.
