e2134109a9f3273b07d6c167bd2fed10899f377c
Max Aubain <max.aubain@gmail.com>
2019-08-26 22:41:16 +0200

    Adds content for developer and reformats profile page for more even element spacing

--- src/Profile.js
+++ src/Profile.js
@@ -24,7 +24,7 @@ const Profile = () => {
           emerging technologies.</p>
         </div>
       </div>
-      <div class="section content">
+      <div class="section-small content">
         <a href="./#project-manager" class="feature-ref-1">
           Project Manager
         </a>
@@ -38,12 +38,16 @@ const Profile = () => {
       <div class="section feature" id="project-manager">
         <div class="feature-name">Project Manager</div>
         <div class="feature-description">
-          <p>I am passionate about project management as a tool to enhance the quality of products and business methods, and to enable developers and engineers to be creative and efficient.  I thrive when working in teams and I am energized by the prospect of bringing structure to the chaos of building something new.  My experience includes Scrum Master in Agile development settings and technical project manager as a Senior Design Engineer.</p>
+          <p>I am passionate about project management as a tool to enhance the quality of products and business methods, and to enable developers and engineers to be creative and efficient.  I thrive when working in teams and I am energized by the prospect of bringing structure to the chaos of building something new.  My experience includes Scrum Master in Agile development settings and technical project management in the semiconductor industry.</p>
         </div>
       </div>
       <div class="section feature" id="full-stack-developer">
         <div class="feature-name">Full Stack Developer</div>
-        <div class="feature-description">I develop.</div>
+        <div class="feature-description">
+          <p>I have written applications in various languages throughout my career for equally as many tasks, from lab test automation to numerical data analysis.</p>
+          <br></br>
+          <p>Now, I am laser focused on web development.  Specifically, I am finishing the final project for a four month bootcamp at Craft Academy in Stockholm.  Opportunites to manage development projects and products, or technical positions that are development-centered, would be my next, ideal career move.</p>
+        </div>
       </div>
       <div class="section feature" id="data-analyst">
         <div class="feature-name">Data Analyst</div>
