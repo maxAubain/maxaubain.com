0a5b5f9b87fcc1462ce869f2a6dfe977a64977d1
Max Aubain <max.aubain@gmail.com>
2019-09-02 11:16:44 +0200

    Adds technologies and full descriptions for the fullstack projects

--- src/components/DeveloperProjects.jsx
+++ src/components/DeveloperProjects.jsx
@@ -2,24 +2,24 @@ import React from "react";
 
 import Projects from "./Projects";
 
-// DeveloperProjects returns instances of the Projects component which contain projects
-//I have worked on, where the return for each project is formatted by the Project component.
+/* DeveloperProjects returns instances of the Projects component which contain my projects,
+where the return for each project is formatted by the Project component. */
 const DeveloperProjects = () => {
   /* Generates references to project information files */
   const fullStackPath = "./src/data/projectsFullStack.json";
   const frontEndPath = "./src/data/projectsFrontEnd.json";
-  const backEndPath = "./src/data/projectsBackEnd.json";
+  /*   const backEndPath = "./src/data/projectsBackEnd.json"; */
   const exercisesPath = "./src/data/projectsExercises.json";
 
   return (
     <>
       <div class="section-title">Full Stack</div>
       <Projects path={fullStackPath} />
-      
+
       <div className="section-title">Front End Applications</div>
       <Projects path={frontEndPath} />
-      
-{/*        <div class="section-title">Back End</div>
+
+      {/*        <div class="section-title">Back End</div>
        <Projects path={backEndPath} /> */}
 
       <div class="section-title">Coding Exercises</div>
