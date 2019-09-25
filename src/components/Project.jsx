a3ad3b7fe396a6e3da6aba8c8dd8556b4a45d35c
Max Aubain <maxaubain@protonmail.com>
2019-09-25 12:49:39 +0200

    Refactors Project for clarity and adds project title color change on show

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,56 +1,54 @@
 import React, { useState } from "react";
 
-import { IconSmall } from "./imageComponents/IconSmall"
+import { IconSmall } from "./imageComponents/IconSmall";
 
 export const Project = props => {
-  const [projectContainerClassName, setProjectContainerClassName] = useState(
-    "project-details-container hidden"
-  );
-  const [projectSummaryTitleClassName, setProjectSummaryTitleClassName] = useState(
-    "project-summary-title"
-  )
-  const [project] = useState(props.project);
+  const [projectAttributes, setProjectAttributes] = useState({
+    hidden: true
+  });
 
-  const descriptionFull = project.descriptionFull.map(paragraph => {
+  let projectContainerClassName, projectSummaryTitleClassName;
+  if (projectAttributes.hidden) {
+    projectContainerClassName = "project-details-container hidden";
+    projectSummaryTitleClassName = "project-summary-title";
+  } else {
+    projectContainerClassName = "project-details-container";
+    projectSummaryTitleClassName = "project-summary-title current";
+  }
+
+  const descriptionFull = props.project.descriptionFull.map(paragraph => {
     return <p key={paragraph}>{paragraph}</p>;
   });
 
-  const handleProjectShow = () => {
-    if (projectContainerClassName == "project-details-container hidden") {
-      setProjectContainerClassName("project-details-container")
-      setProjectSummaryTitleClassName("project-summary-title current")
-    } else {
-      setProjectContainerClassName("project-details-container hidden");
-      setProjectSummaryTitleClassName("project-summary-title")
-    }
-  };
-
   return (
     <>
-      <div className="project-summary" onClick={handleProjectShow}>
-        <IconSmall src={project.image.path} alt={project.image.alt} />
-        <div className={projectSummaryTitleClassName}>{project.title}</div>
+      <div
+        className="project-summary"
+        onClick={() => {setProjectAttributes({ hidden: !projectAttributes.hidden })}}
+      >
+        <IconSmall src={props.project.image.path} alt={props.project.image.alt} />
+        <div className={projectSummaryTitleClassName}>{props.project.title}</div>
         <div className="project-summary-description">
-          {project.descriptionShort}
+          {props.project.descriptionShort}
         </div>
       </div>
 
       <div className={projectContainerClassName}>
         <div className="project-details-padding">
           {descriptionFull}
-          <strong>Technology Stack: {project.tech}</strong>
+          <strong>Technology Stack: {props.project.tech}</strong>
           <div className="project-details-link-container">
-            <a href={project.link1} className="project-details-link">
-              {project.linkLabel1}
+            <a href={props.project.link1} className="project-details-link">
+              {props.project.linkLabel1}
             </a>
-            <a href={project.link2} className="project-details-link">
-              {project.linkLabel2}
+            <a href={props.project.link2} className="project-details-link">
+              {props.project.linkLabel2}
             </a>
-            <a href={project.link3} className="project-details-link">
-              {project.linkLabel3}
+            <a href={props.project.link3} className="project-details-link">
+              {props.project.linkLabel3}
             </a>
-            <a href={project.link4} className="project-details-link">
-              {project.linkLabel4}
+            <a href={props.project.link4} className="project-details-link">
+              {props.project.linkLabel4}
             </a>
           </div>
         </div>
