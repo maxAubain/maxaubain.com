e9de074f10fb8af47a5be7e480c0cdb36c6aee56
Max Aubain <maxaubain@protonmail.com>
2019-12-20 13:56:02 -0500

    Modifies styling for Resume download link

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,13 +4,17 @@ import { WebLink } from "./common/WebLink";
 
 export const Resume = () => {
   return (
-    <>
-      <WebLink url="https://drive.google.com/open?id=1W1EpFB8h9eK1vvrDvxYVNgQ6tspLwpA1" linkText="Download Link" className="weblink" />
+    <div className="resume-wrapper">
+      <h4><WebLink
+        url="https://drive.google.com/open?id=1W1EpFB8h9eK1vvrDvxYVNgQ6tspLwpA1"
+        linkText="Download Link"
+        className="weblink resume-link"
+      /></h4>
       <object
         className="pdf"
         data="./src/pdf/Resume_Developer.pdf"
         type="application/pdf"
       />
-    </>
+    </div>
   );
 };
