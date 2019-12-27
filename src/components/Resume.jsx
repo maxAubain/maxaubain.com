d61cdcdca6dc318b97b3ef3ec7e59cf6c7c18908
Max Aubain <maxaubain@protonmail.com>
2019-12-27 17:18:09 -0500

    Adds new resume pdf file and shareable link

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,13 +6,13 @@ export const Resume = () => {
   return (
     <div className="resume-wrapper">
       <h4><WebLink
-        url="https://drive.google.com/open?id=1W1EpFB8h9eK1vvrDvxYVNgQ6tspLwpA1"
+        url="https://drive.google.com/open?id=1r9NKpSXyXky87jLEICQ_j8X_-7efsXsw"
         linkText="Download Resume"
         className="weblink resume-link"
       /></h4>
       <object
         className="pdf"
-        data="./src/pdf/Resume_Developer.pdf"
+        data="./src/pdf/Max Aubain - R - FSD.pdf"
         type="application/pdf"
       />
     </div>
