12d41a40cbce7453f5fb87163ccb66867f741394
maxAubain <maxaubain@protonmail.com>
2020-02-06 11:46:29 -0500

    Navbar: extracts rendered objs params to hashes, refactors rendered objs to mapped functs, refactors param names for consistency

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,95 +1,113 @@
 import React, { useState, useEffect } from "react";
-
 import { IconSmallPlus } from "../common/IconSmallPlus";
 import { NavLink } from "react-router-dom";
 
-let navLinkClasses = {
-  about: "",
-  portfolio: "",
-  resume: "",
-  contact: ""
-};
+// Handle navLinks and socialMedia parameters
+const navLinks = {
+  "about": {
+    "label": "About",
+    "className": ""
+  },
+  "portfolio": {
+    "label": "Portfolio",
+    "className": ""
+  },
+  "resume": {
+    "label": "Resume",
+    "className": ""
+  },
+  "contact": {
+    "label": "Contact",
+    "className": ""
+  }
+}
+
+const socialMedia = {
+  "github": {
+    "url": "https://github.com/maxAubain",
+    "image": {
+      "path": "./src/img/icon/github.png",
+      "alt": "github-icon"
+    }
+  },
+  "linkedin": {
+    "url": "https://www.linkedin.com/in/maxaubain/",
+    "image": {
+      "path": "./src/img/icon/linkedin.png",
+      "alt": "linkedin-icon"
+    }
+  }
+}
 
 export const NavBar = () => {
-  // Handle navbar shadow
-  const [navBarContainerClass, setNavBarContainerClass] = useState(
+  // Handle navbar shadow on scroll
+  const [navBarContCN, setNavBarContCN] = useState(
     "navbar-container"
   );
+
   const handleNavBarShadow = event => {
     document.documentElement.scrollTop > 10
-      ? setNavBarContainerClass("navbar-container floating-navbar")
-      : setNavBarContainerClass("navbar-container");
+      ? setNavBarContCN("navbar-container floating-navbar")
+      : setNavBarContCN("navbar-container");
   };
+
   useEffect(() => {
     window.addEventListener("scroll", handleNavBarShadow);
   });
 
   // Handle view reset on navlink click
-  const handleViewReset = () =>
+  const handleViewReset = () => {
     window.scrollTo({
       top: 0,
       left: 0,
       behavior: "auto"
     });
+  }
 
-  // Handle Navlink style classes
-  // navLinkClasses are not stored in State to avoid an infinite loop on render
-  Object.keys(navLinkClasses).forEach(key => {
+  // Handle assignment of highlighted style for navLinks dependent on current path
+  Object.keys(navLinks).forEach(key => {
     `/${key}` === location.pathname
-      ? Object.assign(navLinkClasses, { [key]: "navlink-current" })
-      : Object.assign(navLinkClasses, { [key]: "navlink" });
+      ? Object.assign(navLinks[key], { "className": "navlink-current" })
+      : Object.assign(navLinks[key], { "className": "navlink" })
   });
 
+  // Navbar NavLinks object
+  const navLinksObj = Object.keys(navLinks).map(key => {
+    return (
+      <NavLink
+        key={key}
+        className={navLinks[key].className}
+        to={`/${key}`}
+        onClick={handleViewReset}
+      >
+        {navLinks[key].label}
+      </NavLink>
+    )
+  })
+
+  // Social media link/icons object
+  const socialMediaObj = Object.keys(socialMedia).map(key => {
+    return (
+      <a
+        onClick={() => {
+          window.open(`${socialMedia[key].url}`, "_blank");
+        }}
+      >
+        <IconSmallPlus
+          src={socialMedia[key].image.path}
+          alt={socialMedia[key].image.alt}
+        />
+      </a>
+    )
+  })
+
   return (
-    <div className={navBarContainerClass}>
+    <div className={navBarContCN}>
       <div className="navbar-navlinks">
-        <NavLink
-          className={navLinkClasses.about}
-          to="/about"
-          onClick={handleViewReset}
-        >
-          About
-        </NavLink>
-        <NavLink
-          className={navLinkClasses.portfolio}
-          to="portfolio"
-          onClick={handleViewReset}
-        >
-          Portfolio
-        </NavLink>
-        <NavLink
-          className={navLinkClasses.resume}
-          to="resume"
-          onClick={handleViewReset}
-        >
-          Resume
-        </NavLink>
-        <NavLink
-          className={navLinkClasses.contact}
-          to="contact"
-          onClick={handleViewReset}
-        >
-          Contact
-        </NavLink>
+        {navLinksObj}
       </div>
       <div className="navbar-social">
-        <a
-          onClick={() => {
-            window.open("https://github.com/maxAubain", "_blank");
-          }}
-        >
-          <IconSmallPlus src="./src/img/icon/github.png" alt="github icon" />
-        </a>
-        <a
-          onClick={() => {
-            window.open("https://www.linkedin.com/in/maxaubain/", "_blank");
-          }}
-        >
-          <IconSmallPlus
-            src="./src/img/icon/linkedin.png"
-            alt="linkedin icon"
-          />
-        </a>
+        {socialMediaObj}
       </div>
     </div>
   );
