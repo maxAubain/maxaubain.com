000bac13e0d9272bf8c29272b919133eb8328246
maxAubain <maxaubain@protonmail.com>
2020-02-08 11:47:34 -0500

    Exercises: refactors Exercises to contain data import and project object; projectsExercises: updates img paths accordingly

--- src/components/portfolio/Exercises.jsx
+++ src/components/portfolio/Exercises.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { navLinksParams } from "../../router/links";
+import { Project } from "./Project";
 
 export const Exercises = () => {
+  // Get externalLinks data from file containing tech keywords with related weblinks
+  const [externalLinks, setExternalLinks] = useState({});
+  useEffect(() => {
+    if (Object.keys(externalLinks).length === 0) {
+      axios.get("../../src/data/linksExternal.json").then(response => {
+        setExternalLinks(response.data);
+      });
+    }
+  });
+
+  // Get projectsExercises data from file
+  const [exercises, setExercises] = useState([]);
+  useEffect(() => {
+    if (exercises.length === 0) {
+      axios.get(navLinksParams.portfolio.exercises.dataPath).then(response => {
+        setExercises(response.data);
+      });
+    }
+  });
+
+  // Exercises projects object
+  const projects = exercises.map(project => {
+    return (
+      <div key={project.id}>
+        <Project project={project} externalLinks={externalLinks} />
+      </div>
+    )
+  });
+
   return (
-    <></>
+    <>{projects}</>
   )
 }
