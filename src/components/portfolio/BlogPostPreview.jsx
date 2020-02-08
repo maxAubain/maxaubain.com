52441425233331e705dbf6626b24953501ec5f2a
maxAubain <maxaubain@protonmail.com>
2020-02-07 20:49:54 -0500

    Blog, BlogPostPreview: cleans and refactors parameter names and comments for clarity

--- src/components/portfolio/BlogPostPreview.jsx
+++ src/components/portfolio/BlogPostPreview.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 
 export const BlogPostPreview = ({ post }) => {
-  // Date element with 'posted' and 'updated'
+  // Date element with 'posted' and conditional 'revised' values
   let date
-  post.header.date.update === "" ?
-    date = (<>{post.header.date.post}</>)
-    :
-    date = (<>{post.header.date.post} -- last revised {post.header.date.update}</>)
+  post.header.date.update === ""
+    ? date = (<>{post.header.date.post}</>)
+    : date = (<>{post.header.date.post} -- last revised {post.header.date.update}</>)
 
-  // Body paragraphs object
+  // Blog body paragraphs object
+  let key = 0
   const body = post.body.map(bodyObject => {
-    if (Object.keys(bodyObject)[0] === "paragraph") {
-      return (<>{bodyObject.paragraph}</>)
+    if (Object.keys(bodyObject)[0].includes("p")) {
+      key++
+      return (<div key={key}>{bodyObject.paragraph}</div>)
     }
   })
 
+  // Formatted blog post preview object
   return (
     <div className="bpp-container">
       <div className="bpp-title">{post.header.title.main}</div>
