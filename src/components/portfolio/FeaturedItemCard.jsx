bcf4694cfa724ce3197d62a37eac78023ef48367
maxAubain <maxaubain@protonmail.com>
2020-02-12 16:27:17 -0500

    FeaturedItemCard: removes redundant conditional render

--- src/components/portfolio/FeaturedItemCard.jsx
+++ src/components/portfolio/FeaturedItemCard.jsx
@@ -1,16 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { WebLink } from '../common/WebLink'
 
 export const FeaturedItemCard = ({ featuredContentItem }) => {
-  const [loadedImg, setLoadedImg] = useState()
-  const [isImgLoaded, setIsImgLoaded] = useState(false)
-  useEffect(() => {
-    setLoadedImg(
-      <img src={featuredContentItem.image.path} className="featured-img" />
-    )
-    setIsImgLoaded(true)
-  }, [])
-
   const card = (
     <div className="featured-card">
       <div className="featured-category-title-container">
@@ -18,20 +9,17 @@ export const FeaturedItemCard = ({ featuredContentItem }) => {
         <div className="featured-date">{featuredContentItem.date_posted}</div>
         <div className="featured-title">{featuredContentItem.title}</div>
       </div>
-      <div className="featured-img-container">{loadedImg}</div>
+      <div className="featured-img-container">
+        <img src={featuredContentItem.image.path} className="featured-img" />
+      </div>
     </div>
   )
 
   return (
     <>
-      {isImgLoaded && (
-        <div key={featuredContentItem.id} className="featured-container">
-          <WebLink
-            url={featuredContentItem.links['live site']}
-            linkObj={card}
-          />
-        </div>
-      )}
+      <div key={featuredContentItem.id} className="featured-container">
+        <WebLink url={featuredContentItem.links['live site']} linkObj={card} />
+      </div>
     </>
   )
 }
