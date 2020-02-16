c0e99ea58f23ed4f0ab199e7eb097d153dbf8e21
maxAubain <maxaubain@protonmail.com>
2020-02-16 15:33:53 -0500

    BlogPost: adds method to get linked post from array of posts, and method to get body data and return placeholder divs

--- src/components/portfolio/BlogPost.jsx
+++ src/components/portfolio/BlogPost.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
-export const BlogPost = ({}) => {
+export const BlogPost = ({ blogPosts }) => {
   let { blogPostId } = useParams()
 
-  return <>{blogPostId}</>
+  const getPost = blogPosts => {
+    for (let iBlogPosts = 0; iBlogPosts < blogPosts.length; iBlogPosts++) {
+      if (blogPosts[iBlogPosts].relPath == `/${blogPostId}`) {
+        return blogPosts[iBlogPosts]
+      }
+    }
+  }
+  const post = getPost(blogPosts)
+
+  let returnElementKey = -1
+  const postBody = post.body.map(element => {
+    returnElementKey = returnElementKey + 1
+    let elementKey = Object.keys(element)[0]
+    switch (elementKey) {
+      case 'image':
+        return <div key={returnKey}>Image</div>
+      case 'paragraph':
+        return <div key={returnKey}>Paragraph</div>
+      case 'quote':
+        return <div key={returnKey}>Quote</div>
+    }
+  })
+
+  return (
+    <>
+      <div className="bp-container">
+        <div className="bp-body">{postBody}</div>
+      </div>
+    </>
+  )
 }
