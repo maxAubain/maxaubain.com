676cc9307aefb277f5f652b9ada005de5f9fc456
maxAubain <maxaubain@protonmail.com>
2020-02-17 18:25:12 -0500

    BlogPosts, blog-posts: adds styling to BlogPost for paragraphs, quotes, images, and element containers

--- src/components/portfolio/BlogPost.jsx
+++ src/components/portfolio/BlogPost.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
 
 export const BlogPost = ({ blogPosts }) => {
+  // Extract blog post data from array of blog posts by blogPostId
   let { blogPostId } = useParams()
-
   const getPost = blogPosts => {
     for (let iBlogPosts = 0; iBlogPosts < blogPosts.length; iBlogPosts++) {
       if (blogPosts[iBlogPosts].relPath == `/${blogPostId}`) {
@@ -13,23 +13,65 @@ export const BlogPost = ({ blogPosts }) => {
   }
   const post = getPost(blogPosts)
 
-  let returnElementKey = -1
-  const postBody = post.body.map(element => {
-    returnElementKey = returnElementKey + 1
-    let elementKey = Object.keys(element)[0]
-    switch (elementKey) {
+  // Date object
+  let date
+  post.header.date.update === ''
+    ? (date = <>{post.header.date.post}</>)
+    : (date = (
+        <>
+          {post.header.date.post} -- last revised {post.header.date.update}
+        </>
+      ))
+
+  // Render blog post body comprising quotes, images, and paragraphs
+  let returnKey = -1
+  const postBody = post.body.map(bodyElement => {
+    returnKey = returnKey + 1
+    let hashKey = Object.keys(bodyElement)[0]
+    switch (hashKey) {
       case 'image':
-        return <div key={returnKey}>Image</div>
+        return (
+          <div className="bp-image-container" key={returnKey}>
+            <img
+              src={bodyElement.image.src}
+              alt={bodyElement.image.alt}
+              width={bodyElement.image.width}
+            />
+            <div className="bp-image-caption">{bodyElement.image.caption}</div>
+          </div>
+        )
       case 'paragraph':
-        return <div key={returnKey}>Paragraph</div>
+        return (
+          <div className="bp-paragraph-container" key={returnKey}>
+            <p>{bodyElement.paragraph}</p>
+          </div>
+        )
       case 'quote':
-        return <div key={returnKey}>Quote</div>
+        return (
+          <div className="bp-quote-container" key={returnKey}>
+            <p>{bodyElement.quote}</p>
+          </div>
+        )
     }
   })
 
+  // Return formatted blog post
   return (
     <>
+      <div className="bp-navlink-container">
+        <NavLink to="/portfolio/blog" className="navlink">
+          &larr; back to blog
+        </NavLink>
+      </div>
       <div className="bp-container">
+        <div className="bpp-title">{post.header.title.main}</div>
+        <div className="bpp-subtitle">{post.header.title.subtitle}</div>
+        <div className="bpp-date">{date}</div>
+        <img
+          className="bp-header-image"
+          src={post.header.image.src}
+          alt={post.header.image.alt}
+        />
         <div className="bp-body">{postBody}</div>
       </div>
     </>
