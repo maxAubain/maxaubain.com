c40e968834e3715c93e9a924bbfc05e4cb4ad412
maxAubain <maxaubain@protonmail.com>
2020-02-26 19:02:03 -0500

    BlogPostPreviews: adds method to integrate blog-posts-list data to find a given blog post data to populate preview component

--- src/app/portfolio/blog/BlogPostPreviews.jsx
+++ src/app/portfolio/blog/BlogPostPreviews.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { BlogPostPreview } from './BlogPostPreview'
 
-export const BlogPostPreviews = ({ blogPostsList }) => {
+export const BlogPostPreviews = ({ blogPostsList, blogPostsDataPath }) => {
   const blogPostPreviews = blogPostsList.map(post => {
-    let blogPostData = require('../../../global/data/blogPosts/' +
-      post.path +
-      post.id)
+    let blogPostDataPath = blogPostsDataPath + post.path
+    let blogPostData = require('../../../' + blogPostDataPath + '/' + post.id)
     return (
       <div key={post.id}>
-        <BlogPostPreview blogPostData={blogPostData} />
+        <BlogPostPreview
+          blogPostData={blogPostData}
+          blogPostDataPath={blogPostDataPath}
+          postId={post.id}
+        />
       </div>
     )
   })
