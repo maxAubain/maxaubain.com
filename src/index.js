e8ce403108110e02c69c5c7e9628157b54979a50
maxAubain <maxaubain@protonmail.com>
2020-05-05 15:00:42 -0400

    Refactors colors in navlink.scss and weblink.scss to style file partial (_colors.scss)

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import { Top } from './app/top/Top'
-import './app/top/style/fonts/arial'
+import './app/global/style/fonts/arial'
 import './app/global/links/style/navlink'
 import './app/global/links/style/weblink'
-import './app/top/style/global'
+import './app/global/style/global'
 
 ReactDOM.render(<BrowserRouter> <Top /> </BrowserRouter>,
   document.getElementById("app"));
