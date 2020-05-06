7f4589522290bfde6fc7943c613b056b92821edc
maxAubain <maxaubain@protonmail.com>
2020-05-06 09:10:59 -0400

    Refactors style syntax for FeaturedContentCard.jsx component (featured-content-card.scss)

--- src/app/top/portfolio/featured/FeaturedContentCard.jsx
+++ src/app/top/portfolio/featured/FeaturedContentCard.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { WebLink } from '../../../global/links/WebLink'
+import './style/featured-content-card'
 
 export const FeaturedContentCard = ({ featuredContentItem }) => {
   const card = (
-    <div className="featured-card">
-      <div className="featured-category-title-container">
-        <div className="featured-category">{featuredContentItem.category}</div>
-        <div className="featured-date">{featuredContentItem.date_posted}</div>
-        <div className="featured-title">{featuredContentItem.title}</div>
+    <div className="featured-content-card card">
+      <div className="featured-content-card category-title-container">
+        <div className="featured-content-card category">
+          {featuredContentItem.category}
+        </div>
+        <div className="featured-content-card date">
+          {featuredContentItem.date_posted}
+        </div>
+        <div className="featured-content-card title">
+          {featuredContentItem.title}
+        </div>
       </div>
-      <div className="featured-img-container">
+      <div className="featured-content-card img-container">
         <img
           src={require(`${featuredContentItem.image.path}`)}
-          className="featured-img"
+          className="featured-content-card img"
         />
       </div>
     </div>
@@ -20,7 +27,10 @@ export const FeaturedContentCard = ({ featuredContentItem }) => {
 
   return (
     <>
-      <div key={featuredContentItem.id} className="featured-container">
+      <div
+        key={featuredContentItem.id}
+        className="featured-content-card container"
+      >
         <WebLink url={featuredContentItem.links['live site']} linkObj={card} />
       </div>
     </>
