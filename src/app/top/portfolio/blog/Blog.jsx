9b9bdbd0988999c57218a0b10e67bf0afa11f759
maxAubain <maxaubain@protonmail.com>
2020-05-09 12:05:35 -0400

    Refactors style for Blog.jsx component (blog.scss)

--- src/app/top/portfolio/blog/Blog.jsx
+++ src/app/top/portfolio/blog/Blog.jsx
@@ -3,26 +3,29 @@ import { Route, Switch, useRouteMatch } from 'react-router-dom'
 import { BlogPostPreviews } from './BlogPostPreviews'
 import { BlogPost } from './BlogPost'
 import blogPostsList from './data/blog-posts-list'
-const blogPostsDataPath = './data/blogPosts'
+import './style/blog'
 
+const blogPostsDataPath = './data/blogPosts'
 export const Blog = () => {
   let { path } = useRouteMatch()
   return (
-    <>
-      <Switch>
-        <Route exact path={path}>
-          <BlogPostPreviews
-            blogPostsList={blogPostsList}
-            blogPostsDataPath={blogPostsDataPath}
-          />
-        </Route>
-        <Route path={`${path}/:blogPostId`}>
-          <BlogPost
-            blogPostsList={blogPostsList}
-            blogPostsDataPath={blogPostsDataPath}
-          />
-        </Route>
-      </Switch>
-    </>
+    <div className="project-categories-wrapper">
+      <div className="blog container">
+        <Switch>
+          <Route exact path={path}>
+            <BlogPostPreviews
+              blogPostsList={blogPostsList}
+              blogPostsDataPath={blogPostsDataPath}
+            />
+          </Route>
+          <Route path={`${path}/:blogPostId`}>
+            <BlogPost
+              blogPostsList={blogPostsList}
+              blogPostsDataPath={blogPostsDataPath}
+            />
+          </Route>
+        </Switch>
+      </div>
+    </div>
   )
 }
